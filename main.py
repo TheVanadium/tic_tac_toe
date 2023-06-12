6d0c6e7b9825eb36f8ff701b0122c9840edab36b
TheVanadium <53013571+TheVanadium@users.noreply.github.com>
2023-06-12 09:17:14 -0700

    feat: replace former token with dummy token

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ import discord
 from discord.ext import commands
 from bot import bot
 
+TOKEN = 'TOKEN'
+
 if __name__ == '__main__':
-    bot.run('MTExNTY2MDEwNTYxOTM0NTU0OQ.Gpz4TG.oD6LAqM2W6YLvc8UvHM88-3PJ_D4FfFIcC0VmY')
+    bot.run(TOKEN)
